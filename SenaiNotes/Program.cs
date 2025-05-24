81840001345bf0b988a14f38a106ca0ae26fb765
fic <fic@NOTE04-S28>
2025-05-23 21:44:57 -0300

    Configurando a Autorizacao

--- SenaiNotes/Program.cs
+++ SenaiNotes/Program.cs
@@ -1,8 +1,13 @@
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
 using SenaiNotes.Context;
 using SenaiNotes.Interfaces;
 using SenaiNotes.Repositories;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +21,29 @@ builder.Services.AddSwaggerGen(
 options =>
 {
     options.EnableAnnotations();
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer'[space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
 }
 );
 
